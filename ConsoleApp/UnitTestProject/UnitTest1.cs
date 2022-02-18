e4a6c471b2ec0d3c1cfbe875ff1c76114b40cb4a
Padmaraj Thanepatil <padmaraj.thanepatil@globant.com>
2022-02-18 11:29:56 +0530

    First try

---
+++ ConsoleApp/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string expectedResult = "Hello World!";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expectedResult, result);
+            }
+        }
+    }
+}
